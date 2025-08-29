387feb82c2acd9d85537c7f7be9463199a9a7753
Priya-Pathak <39853633+Priya-Pathak@users.noreply.github.com>
2025-08-30 00:22:53 +0530

    Commit to Conv type

--- Activation Functions/relu.py
+++ Activation Functions/relu.py
@@ -37,4 +37,9 @@ relu_function_1.activate(x=32)
 relu_function_1.activate(x=-8)
 
 # Variants of relu:
-# 1. Leaky relu: f(x) = max(0.1x,x)
+# 1. Leaky relu: f(x) = max(0.1x,x) [0.1 value is a hyperparameter like learning rate]
+# 2. Randomized leaky relu : The hyperparameter above is taken randomly from a uniform distribution (0,1)
+#    During the test time we take the average value of upper_limit and lower_limit and use that.
+# 3. Parameteric relu: f(x) = max(ax, x) the model learns the a. It is now a parameter and not a hyper-parameter
+# 4. Exponential Linear Unit: ELU = e(x)-1 when x<0 otherwise x
+# 5. Scaled Exponential Linear Unit: SELU = p[a*e(x)-a when x<0 otherwise x] : Here p and a are fixed
